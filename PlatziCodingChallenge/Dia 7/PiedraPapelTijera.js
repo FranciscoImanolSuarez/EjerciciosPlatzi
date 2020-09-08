9ea99b1e7ea75a90e0eecbc8c4cfa97b4cd74133
Francisco Imanol <imanolsuarez98@gmail.com>
2020-09-08 15:48:27 -0300

    piedra papel y tijera

--- PlatziCodingChallenge/Dia 7/PiedraPapelTijera.js
+++ PlatziCodingChallenge/Dia 7/PiedraPapelTijera.js
@@ -5,15 +5,58 @@
 // partidas?Ejemplo:```
 // ppt(“piedra”, “papel”) ➞ “El ganador es el jugador 2”
 
-function maquina (){
-    const acciones = ['Piedra','Papel','Tijera'];
-    let i = Math.round(Math.random(0,3))
-    return acciones[i];
+//Genera un numero aleatorio entre un rango de enteros
+function aleatorio(minimo, maximo) {
+    var numero = Math.floor(Math.random() * (maximo - minimo + 1) + minimo);
+    return numero;
 }
 
+var piedra = 0;
+var papel = 1;
+var tijera = 2;
 
-const user = prompt('Ingrese un valor')
+var opciones = ["Piedra", "Papel", "Tijera"];
 
-if (user === maquina) {
-    return 'Empate'
+var opcionUsuario;
+var opcionMaquina = aleatorio(0, 2);
+
+opcionUsuario = prompt("¿Que eliges?\nPiedra: 0\nPapel: 1\nTijera: 2", 0);
+
+alert("La maquina eligio:" + opciones[opcionMaquina]);
+
+
+if (opcionUsuario == piedra) {
+    alert("Elegiste Piedra!");
+    if (opcionMaquina == piedra) {
+        alert("Empate!");
+    } else if (opcionMaquina == papel) {
+        alert("Perdiste!");
+    } else if (opcionMaquina == tijera) {
+        alert("Ganaste!");
+    }
+
+} else if (opcionUsuario == papel) {
+    alert("Elegiste Papel!");
+    if (opcionMaquina == piedra) {
+        alert("Ganaste!");
+    } else if (opcionMaquina == papel) {
+        alert("Empate!");
+    } else if (opcionMaquina == tijera) {
+        alert("Perdiste!");
+    }
+
+} else if (opcionUsuario == tijera) {
+    alert("Elegiste Tijera!");
+    if (opcionMaquina == piedra) {
+        alert("Perdiste!");
+    } else if (opcionMaquina == papel) {
+        alert("Ganaste!");
+    } else if (opcionMaquina == tijera) {
+        alert("Empate!");
+    }
+
+} else
+
+{
+    alert("Debes elegir 0,1 ó 2");
 }
--- PlatziCodingChallenge/Dia 8/cilindro.js
+++ PlatziCodingChallenge/Dia 8/cilindro.js
@@ -2,3 +2,16 @@
 // por eso es importante practicarlas constantemente. Un cilindro es un cuerpo geométrico que
 // requiere de varias fórmulas, aplícalas en un programa que reciba datos como su altura y radio
 // de las bases para mostrar el resultado acotado a un decimal.
+
+
+let radio, altura = 0;
+
+// R = Radio 
+// A = Altura
+
+const volumen = (r, a) => {
+    console.log(Math.pi * (Math.pow(r, 2)) * a)
+}
+
+
+volumen(10, 12)
